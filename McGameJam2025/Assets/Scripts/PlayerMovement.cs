d141e8b873823e9f40af85dffdba8ffd4f8e6e9f
YasinPat03 <yasinpatel04@icloud.com>
2025-01-11 14:20:06 -0500

    ADDED ENEMIES AND PATROLLING
    PATROLLING AI

--- McGameJam2025/Assets/Scripts/PlayerMovement.cs
+++ McGameJam2025/Assets/Scripts/PlayerMovement.cs
@@ -29,21 +29,21 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isJumping = false;
-        }
-    }
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Ground"))
+    //     {
+    //         isJumping = false;
+    //     }
+    // }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isJumping = true;
-        }
-    }
+    // private void OnCollisionExit2D(Collision2D collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Ground"))
+    //     {
+    //         isJumping = true;
+    //     }
+    // }
 
 
 }
