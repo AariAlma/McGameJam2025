55337bad48626c2a19d42d54ca4bb393978978cc
YasinPat03 <yasinpatel04@icloud.com>
2025-01-11 23:58:04 -0500

    did some work

--- McGameJam2025/Assets/MonsterMovement.cs
+++ McGameJam2025/Assets/MonsterMovement.cs
@@ -17,15 +17,17 @@ public class MonsterMovement : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
             if(Vector2.Distance(transform.position, patrolPoints[0].position) < 2f)
             {
+                transform.localScale = new Vector3(-3, 3, 1);
                 patrolDestination = 1;
             }
         } 
-
+        
         else if(patrolDestination == 1) 
         {
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
             if(Vector2.Distance(transform.position, patrolPoints[1].position) < 2f)
             {
+                transform.localScale = new Vector3(3, 3, 1);
                 patrolDestination = 0;
             }
         }
